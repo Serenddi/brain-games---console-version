5cb028b4bd0cd1ffad34911e7b9449577656c193
Anastasiia Mikhaleva <dadanastya24342@gmail.com>
2022-10-08 01:00:21 +0700

    feat: write a game odd or even

---
+++ bin/brain-even.js
@@ -0,0 +1,63 @@
+#!/usr/bin/env node
+
+import readlineSync from 'readline-sync';
+
+let random;
+let q1;
+let userName;
+
+function rules() {
+  userName = readlineSync.question('May I have your name? ');
+  console.log(`Hi ${userName}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".')
+}
+
+function randomNum() {
+  random = Math.floor(Math.random() * (40 - 1 + 1) + 1);
+  console.log(`Question: ${random}`);
+  q1 = readlineSync.question('Your answer: ');
+}
+
+let count = 0;
+
+function output() {
+  for (let i = 0; i < 2; i++) {
+    if (random % 2 === 0 && q1 === 'yes') {
+      console.log('Correct!')
+      count++
+      randomNum()
+    } else if (random % 2 !== 0 && q1 === 'no') {
+      console.log('Correct!')
+      count++
+      randomNum()
+    }
+  }
+
+
+  if (count === 2) {
+    console.log(`Congratulations, ${userName}!`)
+  }
+
+
+  if (random % 2 === 0 && q1 === 'no') {
+    console.log(`'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${userName}!`)
+  } else if (random % 2 !== 0 && q1 === 'yes') {
+    console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${userName}!`)
+  }
+
+  if (q1 != 'yes' && q1 != 'no') {
+    console.log('Wrong!')
+  }
+
+}
+
+
+
+function even() {
+  console.log('Welcome to the Brain Games!');
+  rules();
+  randomNum()
+  output()
+}
+
+even()
