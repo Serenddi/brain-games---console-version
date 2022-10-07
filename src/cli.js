f9577f5def86d66d58e5ccc2b2f1e0e75c3a9b19
Anastasiia Mikhaleva <dadanastya24342@gmail.com>
2022-10-08 00:59:26 +0700

    fix: delete imports

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 
 export default function user() {
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hi ${name}!`);
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hi ${userName}!`);
 }
+
+export {userName}
